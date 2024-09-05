84f5465ba4af3f5608a567ddac432a8c4b10ae2b
Jay Rabjohns <jay.rabjohns@flexys.co.uk>
2024-09-05 13:30:34 +0100

    feat: interior mutability of locked value

--- src/spinlock.rs
+++ src/spinlock.rs
@@ -1,22 +1,34 @@
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::{
+    cell::UnsafeCell,
+    collections::btree_map::Values,
+    sync::atomic::{AtomicBool, Ordering},
+};
 
-pub struct SpinLock {
+pub struct SpinLock<T> {
     locked: AtomicBool,
+    value: UnsafeCell<T>,
 }
 
-impl SpinLock {
-    pub const fn new() -> Self {
+// Only spinlock is shared between threads, so interior value doesn't have to be sync
+unsafe impl<T: Send> Sync for SpinLock<T> {}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
         Self {
             locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
         }
     }
 
-    pub fn lock(&self) {
+    pub fn lock(&self) -> &mut T {
         while self.locked.swap(true, Ordering::Acquire) {
             std::hint::spin_loop();
         }
+
+        unsafe { &mut *self.value.get() }
     }
 
+    /// Safety relies on &mut T not being used anymore
     pub fn unlock(&self) {
         self.locked.store(false, Ordering::Release)
     }
